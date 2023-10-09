95cefe984ce21bdb61679560aee0fde5e410f03e
selimRU <selim.soha92@gmail.com>
2023-10-09 23:19:37 +0600

    no data found amended

--- src/components/Orders/Orders.jsx
+++ src/components/Orders/Orders.jsx
@@ -3,18 +3,30 @@ import OrderCard from './OrderCard';
 
 const Orders = () => {
     const [serviceOrdered, setServiceOrdered] = useState([])
+    const [nodataFound, setNodataFound] = useState('')
 
     useEffect(() => {
         const services = JSON.parse(localStorage.getItem('service'))
-        setServiceOrdered(services)
+        if (services) {
+            setServiceOrdered(services)
+        }
+        else {
+            setNodataFound('No Data Found')
+        }
     }, [])
     return (
-        <div className=' max-w-6xl mx-auto py-16 gap-10 grid lg:grid-cols-3'>
+        <div className=' max-w-6xl mx-auto'>
             {
-                serviceOrdered.map(orderedItem => <OrderCard
-                    key={orderedItem.id}
-                    orderedItem={orderedItem}
-                ></OrderCard>)
+                nodataFound ? <p className=' text-center text-2xl my-10 h-[350px]'>{nodataFound}</p> :
+
+                    <div className=' max-w-6xl mx-auto py-16 gap-10 grid lg:grid-cols-3'>
+                        {
+                            serviceOrdered.map(orderedItem => <OrderCard
+                                key={orderedItem.id}
+                                orderedItem={orderedItem}
+                            ></OrderCard>)
+                        }
+                    </div>
             }
         </div>
     );
